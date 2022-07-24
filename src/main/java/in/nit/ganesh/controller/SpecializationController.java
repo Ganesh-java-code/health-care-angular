16aa309fa4315d3e2453d167f96720e4b4627c3d
Ganesh Patil <gp302999@gmail.com>
2022-07-24 09:18:05 +0530

    second commit with features branch to spec

--- src/main/java/in/nit/ganesh/controller/SpecializationController.java
+++ src/main/java/in/nit/ganesh/controller/SpecializationController.java
@@ -1,16 +1,29 @@
 package in.nit.ganesh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.nit.ganesh.entity.Specialization;
+import in.nit.ganesh.exception.SpecializationException;
+import in.nit.ganesh.service.SpecializationService;
 
 @RestController
 @RequestMapping("/spec")
 public class SpecializationController {
 
-	@GetMapping("/show")
-	public String showData() {
-		return "Hello world!!!";
-	}
+	@Autowired
+	private SpecializationService service;
 	
+	@GetMapping("/show")
+	public ResponseEntity<String> saveSpecialization(@RequestBody Specialization specialization){
+		ResponseEntity<String> resp = null;
+		Integer id = service.saveSpecialization(specialization);
+		resp = new ResponseEntity<String>(HttpStatus.CREATED);
+		return resp;
+	}
 }
---
+++ src/main/java/in/nit/ganesh/exception/SpecializationException.java
@@ -0,0 +1,18 @@
+package in.nit.ganesh.exception;
+
+public class SpecializationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SpecializationException() {
+		super();
+	}
+	
+	public SpecializationException(String message) {
+		super(message);
+	}
+	
+}
